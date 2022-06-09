653707177c0f2cfab2d11cd39710da922c248bd3
crissdot <cris-2-11-2001@hotmail.com>
2022-06-08 20:48:49 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola Mundo');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000
+  });
+});
+
+app.listen(port, () => {
+  console.log('Escuchando en el puerto: ' + port);
+});
