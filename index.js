b82cf6a2b42c4be4577d8b6bfccfbe544d96ce8c
crissdot <cris-2-11-2001@hotmail.com>
2022-06-09 19:46:50 -0500

    refactor routes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const faker = require('faker');
+const routerApi = require('./routes/index.router');
 
 const app = express();
 const port = 3000;
@@ -8,55 +8,7 @@ app.get('/', (req, res) => {
   res.send('Hola Mundo');
 });
 
-app.get('/products/estatico', (req, res) => {
-  res.send('I am not dynamic');
-});
-
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-
-  let index = 0;
-  for(index; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-
-  res.json(products);
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'Product 2',
-    price: 2000
-  });
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if(limit && offset) {
-    res.json({
-      limit,
-      offset
-    });
-  } else {
-    res.send('No hay suficientes parámetros');
-  };
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId
-  });
-});
+routerApi(app);
 
 app.listen(port, () => {
   console.log('Escuchando en el puerto: ' + port);
