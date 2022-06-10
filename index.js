e24697d3a385d0b5753729ad6dce7a34d1376ac2
crissdot <cris-2-11-2001@hotmail.com>
2022-06-09 19:12:16 -0500

    request params

--- index.js
+++ index.js
@@ -7,9 +7,32 @@ app.get('/', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000
+    },
+    {
+      name: 'Product 2',
+      price: 2000
+    },
+  ]);
+});
+
+app.get('/productos/:id', (req, res) => {
+  const { id } = req.params;
   res.json({
-    name: 'Product 1',
-    price: 1000
+    id,
+    name: 'Product 2',
+    price: 2000
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId
   });
 });
 
