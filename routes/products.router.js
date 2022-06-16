058a5d34049124446761a8bfb81bd0b459a3bb16
crissdot <cris-2-11-2001@hotmail.com>
2022-06-16 14:25:59 -0500

    add validator joi

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const ProductsService = require('../services/products.service.js');
+const { validatorHandler } = require('../middlewares/validator.handler');
+const { getProductSchema, createProductSchema, updateProductSchema } = require('../schemas/products.schema');
 
 const router = express.Router();
 const service = new ProductsService();
@@ -10,41 +12,54 @@ router.get('/', (req, res, next) => {
   res.json(products);
 });
 
-router.get('/:id', (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const product = service.findOne(id);
-    res.json(product);
-  } catch (error) {
-    next(error);
+router.get('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const product = service.findOne(id);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-router.post('/', (req, res) => {
-  const body = req.body;
-  const newProduct = service.create(body);
-  res.status(201).json(newProduct);
-});
-
-router.patch('/:id', (req, res, next) => {
-  try {
-    const { id } = req.params;
+router.post('/',
+  validatorHandler(createProductSchema, 'body'),
+  (req, res) => {
     const body = req.body;
-    const product = service.update(id, body);
-    res.json(product);
-  } catch (error) {
-    next(error);
-  }
-});
+    const newProduct = service.create(body);
+    res.status(201).json(newProduct);
+}
+);
 
-router.delete('/:id', (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const deletedProduct = service.delete(id);
-    res.json(deletedProduct);
-  } catch (error) {
-    next(error);
+router.patch('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const product = service.update(id, body);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
+
+router.delete('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const deletedProduct = service.delete(id);
+      res.json(deletedProduct);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
