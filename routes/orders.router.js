464b8243873383c3e973e01c6a4e47479f17207a
crissdot <cris-2-11-2001@hotmail.com>
2022-06-25 08:51:31 -0500

    add order item

--- routes/orders.router.js
+++ routes/orders.router.js
@@ -38,4 +38,18 @@ router.post(
   }
 );
 
+router.post(
+  '/add-item',
+  validatorHandler(addItemSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newItem = await service.addItem(body);
+      res.status(201).json(newItem);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 module.exports = router;
