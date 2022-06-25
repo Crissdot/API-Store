638dc2ad43be08556cfbff4e2a1208ec7d2967cd
crissdot <cris-2-11-2001@hotmail.com>
2022-06-25 07:58:31 -0500

    add relation products categories

--- schemas/products.schema.js
+++ schemas/products.schema.js
@@ -3,6 +3,7 @@ const Joi = require('joi');
 const id = Joi.string().uuid();
 const name = Joi.string().min(3).max(15);
 const price = Joi.number().integer().min(10);
+const description = Joi.string().min(10);
 const image = Joi.string().uri();
 
 const getProductSchema = Joi.object({
@@ -13,12 +14,14 @@ const createProductSchema = Joi.object({
   name: name.required(),
   price: price.required(),
   image: image.required(),
+  description: description.required(),
 });
 
 const updateProductSchema = Joi.object({
   name,
   price,
   image,
+  description,
 });
 
 module.exports = { getProductSchema, createProductSchema, updateProductSchema }
